976d26e27a9bd18bea601ad97e3e697e6923533d
jubobs <jubobs-oss@protonmail.com>
2022-09-15 07:56:31 +0200

    Reset cmd to just after using go keyword

--- src/namecheck/cmd/cli/main.go
+++ src/namecheck/cmd/cli/main.go
@@ -12,22 +12,6 @@ import (
 	"github.com/jub0bs/namecheck/twitter"
 )
 
-type Status int
-
-type Result struct {
-	Username  string
-	Platform  string
-	Valid     bool
-	Available bool
-}
-
-const (
-	Unknown Status = iota
-	Active
-	Suspended
-	Available
-)
-
 func main() {
 	if len(os.Args[1:]) == 0 {
 		log.Fatal("username args is required")
@@ -44,55 +28,28 @@ func main() {
 		}
 		checkers = append(checkers, t, g)
 	}
-	results := make(chan Result, len(checkers))
-	errc := make(chan error, len(checkers))
 	var wg sync.WaitGroup
 	wg.Add(len(checkers))
 	for _, checker := range checkers {
-		go check(checker, username, &wg, results, errc)
+		go check(checker, username, &wg)
 	}
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	for {
-		select {
-		case err := <-errc:
-			const tmpl = "namecheck: some error occurred: %s\n"
-			fmt.Fprintf(os.Stderr, tmpl, err)
-			return
-		case res, ok := <-results:
-			if !ok {
-				return
-			}
-			fmt.Println(res)
-		}
-	}
+	wg.Wait()
 }
 
-func check(
-	checker namecheck.Checker,
-	username string,
-	wg *sync.WaitGroup,
-	results chan<- Result,
-	errc chan<- error,
-) {
+func check(checker namecheck.Checker, username string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	res := Result{
-		Username: username,
-		Platform: checker.String(),
-	}
-	res.Valid = checker.IsValid(username)
-	if !res.Valid {
-		results <- res
+	if !checker.IsValid(username) {
+		fmt.Printf("%q is not valid on %s\n", username, checker.String())
 		return
 	}
 	avail, err := checker.IsAvailable(username)
 	if err != nil {
-		errc <- err
+		fmt.Fprint(os.Stderr, err)
 		return
 	}
-	res.Available = avail
-	results <- res
+	if !avail {
+		fmt.Printf("%q is valid but unavailable on %s\n", username, checker.String())
+		return
+	}
+	fmt.Printf("%q is valid and available on %s\n", username, checker.String())
 }
